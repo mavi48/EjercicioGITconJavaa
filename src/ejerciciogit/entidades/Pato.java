a625e61119c62deb451e77aaf85a0cf8493045b1
RobertClaros <audioprobolivia@gmail.com>
2023-09-26 22:36:45 -0400

    se creo la entidad PAto

---
+++ src/ejerciciogit/entidades/Pato.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciogit.entidades;
+
+/**
+ *
+ * @author RobertClaros
+ */
+public class Pato {
+    private String nombre;
+    
+}
